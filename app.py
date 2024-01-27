7e6301b6c0774b499b14c0e19d89387b5cc2c12f
sudokube <@>
2024-01-27 03:38:06 -0800

    Add upload function in the index.html

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -7,6 +8,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():  # put application's code here
     return render_template("index.html")
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    file.save(file.filename)
 
 
 if __name__ == '__main__':
