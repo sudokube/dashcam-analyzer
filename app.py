65d5e6a3c1a3e3e429bc7f3b20ca39e140ebfab9
sudokube <@>
2024-01-29 19:13:44 -0800

    Add subprocess for detection

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import subprocess
+import uuid
 
 app = Flask(__name__)
 
@@ -12,8 +14,18 @@ def index():  # put application's code here
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    file = request.files['file']
-    file.save(file.filename)
+    uploaded_file = request.files['file']
+
+    if uploaded_file.filename != '':
+        file_path = f'./upload/{str(uuid.uuid4()) + ".mp4"}'
+        uploaded_file.save(file_path)
+
+        module_path = './modules/going/detect.py'
+        project_path = './temp'
+        weights_path = './modules/going/runs/train/exp12/weights/best.pt'
+
+        command = f'python {module_path} --project {project_path} --source {file_path} --weights {weights_path}'
+        subprocess.run(command, shell=True)
 
 
 if __name__ == '__main__':
